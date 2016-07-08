346f750efb1169e4262f8cd7ab4dcd746a6b5556
Alex Devksh <alekseydevksh@gmail.com>
2016-07-08 13:34:32 +0300

    merged before search block

--- spaceawe/settings.py
+++ spaceawe/settings.py
@@ -155,8 +155,8 @@ WSGI_APPLICATION = 'spaceawe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'spaceawe.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spaceawe',
         'USER': secrets['DATABASE_USER_PROD'],
         'PASSWORD': secrets['DATABASE_PASSWORD_PROD'],
         'HOST': '127.0.0.1',
